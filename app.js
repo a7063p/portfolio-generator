351cf4b406422466432149b92a604fd060e3eacb
andrew <ap@ampiusa.com>
2021-02-07 11:29:57 -0800

    update src dir

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const profileDataArgs = process.argv.slice(2);
 
 const printProfileData = profileDataArr => {
@@ -6,10 +7,26 @@ const printProfileData = profileDataArr => {
     console.log(profileDataArr[i]);
   }
 
-  console.log('================');
+ 
 
-  // Is the same as this...
   profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 
 printProfileData(profileDataArgs);
+
+// const generatePage = (userName, githubName) => {
+//   return `
+//   <!DOCTYPE html>
+//   <html lang='en'>
+//   <head>
+//       <meta charset="UTF-8">
+//       <meta name='viewport' content='width=device-width, initial-scaled=1.0'>
+//       <meta http-equiv="X-UA-Compatable" content='ie-edge'>
+//       <title> Portfolio Demo</title>
+//   </head>
+//   <body>
+//       <h1>${name}</h1>
+//       <h2><a href="https//github.com/${github}">Github</a></h2>
+//   </body>
+//   </html>
+//    `;}
